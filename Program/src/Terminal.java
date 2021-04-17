456838a2068c03e50cadaea2c54e10c339c12da0
watterkill <marek@sinovi.info>
2021-04-17 14:18:36 +0200

    terminal issue repaired

--- Program/src/Terminal.java
+++ Program/src/Terminal.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+
 public class Terminal {
+    private JTextField textField1;
+    private JLabel cs;
+    private JPanel pan;
+    private JTextPane textPane1;
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        Terminal t= new Terminal();
+        frame.setContentPane(t.pan);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(500, 300);
+        frame.setVisible(true);
+    }
+
+
 }
