cb0683fb4f898d307256433bc6577b5ba2ec3f06
uzivatel <gumzub1@gmail.com>
2021-04-19 12:57:32 +0200

    passwords prepared

--- Program/src/computer/program/logging/Passwd.java
+++ Program/src/computer/program/logging/Passwd.java
@@ -1,11 +1,44 @@
 package computer.program.logging;
 
+
+
 import computer.File;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Passwd extends File {
+    ArrayList<User>users=new ArrayList<>();
     public Passwd() {
-        super("passwd",File.system);
+        super("passwd",system);
     }
+
+    public String generatePassword(){
+        ArrayList<String>passwords=new ArrayList<>();
+        try {
+            Scanner scnr=new Scanner(new java.io.File(getClass().getResource("/computer/program/logging/passwords").getFile()));
+            while (scnr.hasNext()){
+                passwords.add(scnr.next());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return passwords.get(ThreadLocalRandom.current().nextInt(0,passwords.size()));
+    }
+
+    public String createString(){
+        StringBuilder ret= new StringBuilder();
+        for (User u:users){
+            ret.append(u.name).append(":").append(u.password).append("\n");
+        }
+        return ret.toString();
+    }
+
+
+
+
 
 
 }
